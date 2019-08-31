4161c1518c9cdad32c1a0427efbddf3438110401
Destiny Sopha <destiny@sopha.org>
2019-08-31 13:31:12 -0400

    Added functionality to display a flower description from Wikipedia

--- What Flower/Controllers/ViewController.swift
+++ What Flower/Controllers/ViewController.swift
@@ -9,11 +9,15 @@
 import UIKit
 import CoreML
 import Vision
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
   
+  let wikipediaURL = "https://en.wikipedia.org/w/api.php"
   let imagePicker = UIImagePickerController()
   
+  @IBOutlet weak var label: UILabel!
   @IBOutlet weak var imageView: UIImageView!
 
   override func viewDidLoad() {
@@ -52,9 +56,12 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
     
     let request = VNCoreMLRequest(model: model) { (request, error) in
-      let classification = request.results?.first as? VNClassificationObservation
+      guard let classification = request.results?.first as? VNClassificationObservation else {
+        fatalError("Could not classify image.")
+      }
       
-      self.navigationItem.title = classification?.identifier.capitalized
+      self.navigationItem.title = classification.identifier.capitalized
+      self.requestWikiInfo(flowerName: classification.identifier)
 
     }
   
@@ -68,6 +75,36 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     print(error)
   }
   
+  }
+  
+  func requestWikiInfo(flowerName: String) {
+    
+    let parameters : [String:String] = [
+      "format" : "json",
+      "action" : "query",
+      "prop" : "extracts",
+      "exintro" : "",
+      "explaintext" : "",
+      "titles" : flowerName,
+      "indexpageids" : "",
+      "redirects" : "1",
+    ]
+    
+    Alamofire.request(wikipediaURL, method: .get, parameters: parameters).responseJSON { (response) in
+      if response.result.isSuccess {
+        print("Got the Wikipedia information.")
+        print(response)
+        
+        let flowerJSON : JSON = JSON(response.result.value!)
+        
+        let pageID = flowerJSON["query"]["pageids"][0].stringValue
+        
+        let flowerDescription = flowerJSON["query"]["pages"][pageID]["extract"].stringValue
+        
+        self.label.text = flowerDescription
+        
+      }
+    }
   }
   
   @IBAction func cameraButtonTapped(_ sender: UIBarButtonItem) {
